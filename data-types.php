1428d2619dcf26b2dbfb72acfd811a94056c461c
sachinkumarnke <your-sachinkumarnke7@gmail.com>
2026-01-08 18:14:04 +0530

    Add data types demonstration in PHP

---
+++ data-types.php
@@ -0,0 +1,50 @@
+<?php
+// All Data types in PHP
+// string
+// integer
+// float
+// boolean
+//null
+// array
+// object(Class)
+// resource
+
+//$name="sachin kumar K";
+//echo $name;
+
+
+$name="sachin kumar K";
+//echo var_dump($name);
+
+
+//$num=50;
+//echo $num;
+
+$num=100;
+//echo var_dump($num);
+
+
+$numFloat=10.10;
+//echo var_dump($numFloat);
+
+$bull=true;
+
+// echo var_dump($bull);
+
+$array=["sachin","alix","sonu",10];
+
+//echo var_dump($array);
+
+
+$empty=null;
+//echo var_dump($empty);
+
+
+// $user = new ClassName();
+
+//resource
+$connection = ftp_connect("127.0.0.1") or die("local host not found");
+echo var_dump($connection);
+
+
+?>
