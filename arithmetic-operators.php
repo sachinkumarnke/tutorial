290a9cc79f3ffc92a146ebdae5fe02ba04cfbdf6
sachinkumarnke <your-sachinkumarnke7@gmail.com>
2026-01-08 21:30:22 +0530

    Add arithmetic operations example in Arithmetic-operators.php

---
+++ arithmetic-operators.php
@@ -0,0 +1,20 @@
+<?php
+
+$a=20;
+$b=50;
+//echo $a+$b;
+//echo $a - $b;
+//echo $a * $b;
+//echo $b / $a;
+//echo $b % $a;
+//echo $a**$b; // Exponentiation operator
+
+//echo 2**2;
+
+//echo 2*2*2*2*2;
+
+const num1=50;
+const num2=100;
+echo num1*num2;
+
+?>
