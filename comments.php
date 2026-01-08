942c01335fdf9712b2019a1c4680642137e4de0c
sachinkumarnke <your-sachinkumarnke7@gmail.com>
2026-01-08 18:31:08 +0530

    Add comments and user information in comments.php

---
+++ comments.php
@@ -0,0 +1,19 @@
+<?php
+
+$name="sachin kumar";
+//$city="patna";  // City name
+// $age="23";    // Age of person
+echo $name;
+
+?>
+
+<?php
+/*
+user information
+$name="sachin kumar";
+$city="patna";  // City name
+*/
+ $age="23";    // Age of person
+echo $name;
+
+?>
