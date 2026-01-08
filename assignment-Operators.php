dbccb7516811c27e20ffe8bcb3df8dde5a4c2798
sachinkumarnke <your-sachinkumarnke7@gmail.com>
2026-01-08 21:48:59 +0530

    Add assignment operators demonstration in assignment-Operators.php

---
+++ assignment-Operators.php
@@ -0,0 +1,34 @@
+<?php
+
+$a=50;
+$b=60;
+
+//$a=$b;
+
+// $a= $a+$b; Assignment operators;
+// $a+=$b;     //Assignment operators with addition
+
+
+// $a-=$b;   //Assignment operators with subtraction
+// $a= $a-$b;  
+
+
+// $a=$a*$b;   //Assignment operators with multiplication
+// $a*=$b;
+
+// $a= $a/$b;   //Assignment operators with division
+// $a/=$b;
+
+// $a= $a%$b;   //Assignment operators with modulus
+// $a%=$b;
+
+// $a= $a**$b;   //Assignment operators with Exponentiation
+// $a**=$b;
+
+
+echo $a;
+
+
+
+
+?>
