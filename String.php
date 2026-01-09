e16be286cba5ac88cb66e6e59082e993b13118e4
sachinkumarnke <your-sachinkumarnke7@gmail.com>
2026-01-09 12:27:50 +0530

    Add string manipulation examples in String.php

---
+++ String.php
@@ -0,0 +1,22 @@
+<?php
+
+$name="sachin kumar";
+$age=24;
+// $string='sonu';
+// $str1="50";
+// $str2="ram is the best";
+
+
+// echo "My name is Sachin Kumar " .$name; // double quote me variable ko print kr skte
+// echo 'My name is Sachin Kumar '.$name; //single quote me variable ko print nhi kr skte
+ 
+
+// echo 'my name is ' .$name. ' and my age is '.$age;  // concatenation operator(.) use krke string ko jod skte hai
+
+$str="hello how are you "; // string variable
+$str1="i am fine thank you"; 
+$str.=$str1; // concatenation assignment operator(.=) use krke string ko jod skte hai
+echo $str; 
+
+
+?>
