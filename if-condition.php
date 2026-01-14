f39899c2dc295a10dad467fd9cbf25cc49e3aa94
sachinkumarnke <your-sachinkumarnke7@gmail.com>
2026-01-14 09:47:37 +0530

    Add if-condition example with user name validation in if-condition.php

---
+++ if-condition.php
@@ -0,0 +1,20 @@
+<?php
+
+$age=50;
+
+// if ($age>300){
+//     echo "welcome to this websit"; 
+// }else{
+//     echo "you are under age";
+// }
+
+
+$name="kumar";
+if($name=="sachin kumar"){
+    echo "welcome to this website $name ";
+}else{
+    echo" $name this user is not present is our Database";
+}
+
+
+?>
