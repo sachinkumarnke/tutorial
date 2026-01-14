d0d753646c9537e36aae3fdbfdf39d82cdf3fd00
sachinkumarnke <your-sachinkumarnke7@gmail.com>
2026-01-14 10:12:22 +0530

    Add switch statement example in switch_Statement.php

---
+++ switch_Statement.php
@@ -0,0 +1,22 @@
+<?php
+
+// switch statement example
+
+$favoritColor="black";
+switch($favoritColor){
+    case"red";
+    echo"red color is my favorite color";
+    break;
+    case"blue";
+    echo"blue color is my friends favorite color";
+    case"green";
+    echo"green color is my brother favorite color";
+    break;
+    default:
+    echo"this color is not found in our database";
+}
+
+
+
+
+?>
