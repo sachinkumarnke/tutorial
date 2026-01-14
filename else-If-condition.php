be2ae06effab1503590a12d655273218e5babee7
sachinkumarnke <your-sachinkumarnke7@gmail.com>
2026-01-14 10:03:30 +0530

    Add else-if condition example for country classification in else-If-condition.php

---
+++ else-If-condition.php
@@ -0,0 +1,19 @@
+<?php
+
+$country="Japan";
+if($country=="india"){
+    echo "this is an asion country";
+}elseif($country=="France"){
+echo "this is a eurpean country";
+}elseif($country=="USA"){
+    echo "this is american country";
+}
+
+
+else{
+    echo "unknown counter";
+}
+
+
+
+?>
